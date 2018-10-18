b830cb9878082874167c257dcd1dbd1583951022
Chandra <Risheek.G.Chandra@CenturyLink.com>
2018-10-18 20:42:16 +0530

    Sql dump

---
+++ sql_dumps/cars.sql
@@ -0,0 +1,44 @@
+--------------------------------------------------------
+--  File created - Thursday-October-18-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table CUSTOMER_CARS_RI
+--------------------------------------------------------
+
+  CREATE TABLE "TRAINING"."CUSTOMER_CARS_RI" 
+   (	"CID" NUMBER, 
+	"REGNO" VARCHAR2(12 BYTE), 
+	"MODELNAME" VARCHAR2(25 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "TRAINING" ;
+REM INSERTING into TRAINING.CUSTOMER_CARS_RI
+SET DEFINE OFF;
+Insert into TRAINING.CUSTOMER_CARS_RI (CID,REGNO,MODELNAME) values (3,'KA01A1234','Omni');
+Insert into TRAINING.CUSTOMER_CARS_RI (CID,REGNO,MODELNAME) values (3,'KA01A1235','esteem');
+Insert into TRAINING.CUSTOMER_CARS_RI (CID,REGNO,MODELNAME) values (3,'KA01A1236','Omni');
+--------------------------------------------------------
+--  DDL for Index SYS_C0094894
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "TRAINING"."SYS_C0094894" ON "TRAINING"."CUSTOMER_CARS_RI" ("REGNO") 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "TRAINING" ;
+--------------------------------------------------------
+--  Constraints for Table CUSTOMER_CARS_RI
+--------------------------------------------------------
+
+  ALTER TABLE "TRAINING"."CUSTOMER_CARS_RI" ADD PRIMARY KEY ("REGNO")
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT)
+  TABLESPACE "TRAINING"  ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table CUSTOMER_CARS_RI
+--------------------------------------------------------
+
+  ALTER TABLE "TRAINING"."CUSTOMER_CARS_RI" ADD FOREIGN KEY ("CID")
+	  REFERENCES "TRAINING"."SERVICE_CUSTOMER_RI" ("CID") ENABLE;
